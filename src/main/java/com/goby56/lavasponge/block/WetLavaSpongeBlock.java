0f7bc4993b459ad9e3c2d4cf7babe20cb7dcef17
Goby56 <60710855+Goby56@users.noreply.github.com>
2022-06-11 15:05:27 +0200

    Wet lava sponge evaporates water

--- src/main/java/com/goby56/lavasponge/block/WetLavaSpongeBlock.java
+++ src/main/java/com/goby56/lavasponge/block/WetLavaSpongeBlock.java
@@ -1,16 +1,22 @@
 package com.goby56.lavasponge.block;
 
 import com.goby56.lavasponge.world.ModWorldEvents;
+import com.google.common.collect.Lists;
 import net.minecraft.block.*;
+import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.fluid.FluidState;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.tag.FluidTags;
+import net.minecraft.util.Pair;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import net.minecraft.world.WorldEvents;
+
+import java.util.LinkedList;
 
 public class WetLavaSpongeBlock extends Block {
 
@@ -24,6 +30,11 @@ public class WetLavaSpongeBlock extends Block {
             return;
         }
         this.update(world, pos);
+        if (this.absorbWater(world, pos)) {
+            world.syncWorldEvent(WorldEvents.BLOCK_BROKEN, pos, Block.getRawIdFromState(Blocks.WATER.getDefaultState()));
+            world.syncWorldEvent(ModWorldEvents.WET_LAVA_SPONGE_EVAPORATES_WATER, pos, 0);
+            world.playSound(null, pos, SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS, 1.0f, (1.0f + world.getRandom().nextFloat() * 0.2f) * 0.7f);
+        }
     }
 
     @Override
@@ -69,5 +80,46 @@ public class WetLavaSpongeBlock extends Block {
             world.syncWorldEvent(ModWorldEvents.WET_LAVA_SPONGE_COOLS_DOWN, pos, 0);
             world.playSound(null, pos, SoundEvents.BLOCK_FIRE_EXTINGUISH, SoundCategory.BLOCKS, 1.0f, (1.0f + world.getRandom().nextFloat() * 0.2f) * 0.7f);
         }
+    }
+
+    private boolean absorbWater(World world, BlockPos pos) {
+        LinkedList<Pair<BlockPos, Integer>> queue = Lists.newLinkedList();
+        queue.add(new Pair<BlockPos, Integer>(pos, 0));
+        int i = 0;
+        while (!queue.isEmpty()) {
+            Pair pair = (Pair)queue.poll();
+            BlockPos blockPos = (BlockPos)pair.getLeft();
+            int j = (Integer)pair.getRight();
+            for (Direction direction : Direction.values()) {
+                BlockPos blockPos2 = blockPos.offset(direction);
+                BlockState blockState = world.getBlockState(blockPos2);
+                FluidState fluidState = world.getFluidState(blockPos2);
+                Material material = blockState.getMaterial();
+                if (!fluidState.isIn(FluidTags.WATER)) continue;
+                if (blockState.getBlock() instanceof FluidDrainable && !((FluidDrainable)((Object)blockState.getBlock())).tryDrainFluid(world, blockPos2, blockState).isEmpty()) {
+                    ++i;
+                    if (j >= 2) continue;
+                    queue.add(new Pair<BlockPos, Integer>(blockPos2, j + 1));
+                    continue;
+                }
+                if (blockState.getBlock() instanceof FluidBlock) {
+                    world.setBlockState(blockPos2, Blocks.AIR.getDefaultState(), Block.NOTIFY_ALL);
+                    ++i;
+                    if (j >= 2) continue;
+                    queue.add(new Pair<BlockPos, Integer>(blockPos2, j + 1));
+                    continue;
+                }
+                if (material != Material.UNDERWATER_PLANT && material != Material.REPLACEABLE_UNDERWATER_PLANT) continue;
+                BlockEntity blockEntity = blockState.hasBlockEntity() ? world.getBlockEntity(blockPos2) : null;
+                SpongeBlock.dropStacks(blockState, world, blockPos2, blockEntity);
+                world.setBlockState(blockPos2, Blocks.AIR.getDefaultState(), Block.NOTIFY_ALL);
+                ++i;
+                if (j >= 2) continue;
+                queue.add(new Pair<BlockPos, Integer>(blockPos2, j + 1));
+            }
+            if (i <= 64) continue;
+            break;
+        }
+        return i > 0;
     }
 }
--- src/main/java/com/goby56/lavasponge/world/ModWorldEvents.java
+++ src/main/java/com/goby56/lavasponge/world/ModWorldEvents.java
@@ -6,6 +6,12 @@ public class ModWorldEvents {
      * A wet lava sponge cools down on top of blue ice. Spawns cloud particles.
      * Called by WetLavaSpongeBlock#onBlockAdded
      */
-    public static final int WET_LAVA_SPONGE_COOLS_DOWN = 2009;
+    public static final int WET_LAVA_SPONGE_COOLS_DOWN = 5609;
+
+    /**
+     * A wet lava sponge is placed in water which evaporates. Spawn cloud particles.
+     * Called by WetLavaSpongeBlock#onBlockAdded
+     */
+    public static final int WET_LAVA_SPONGE_EVAPORATES_WATER = 5610;
 
 }
